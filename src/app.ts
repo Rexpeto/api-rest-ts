843210946409e1e1dc47a6e3d5ce23db695dc034
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-25 16:08:08 -0400

    Use router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,3 +11,5 @@ app.use(cors());
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
+
+app.use(router);
