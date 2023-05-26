effdfbb5f6a4e330237d2066e928b65bf44cbaed
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-26 19:48:25 -0400

    Register user

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import { newUser } from "../services/auth";
+import { handleHttp } from "../utils/error.handle";
 
-export const registerCrtl = async (req: Request, res: Response) => {
+export const registerCrtl = async ({ body }: Request, res: Response) => {
+    const { email, password } = body;
 
+    try {
+        if (!email) return res.status(404).json({ msg: "Debe colocar un email" });
+        if (!password) return res.status(404).json({ msg: "Debe colocar una contraseña" });
+
+        const response = await newUser(body);
+        res.status(200).json(response);
+    } catch (e) {
+        handleHttp(res, "Email o Contraseña inválida", e);
+    }
 }
