6d18d40d2ff0fe0a225027ceb503b612e546cd7e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-26 14:32:42 -0400

    Service getCars

--- src/services/item.ts
+++ src/services/item.ts
@@ -5,3 +5,9 @@ export const insertItem = async (item: Car) => {
     const responseInsert = await ItemModel.create(item);
     return responseInsert;
 };
+
+export const getCars = async () => {
+    const response = await ItemModel.find();
+
+    return response;
+};
