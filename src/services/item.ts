5041b98b983ffdae58924ecf05a10a13748e8572
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-26 14:33:30 -0400

    insertItem -> insertCar

--- src/services/item.ts
+++ src/services/item.ts
@@ -1,7 +1,7 @@
 import { Car } from "../interfaces/car.interface";
 import ItemModel from "../models/item";
 
-export const insertItem = async (item: Car) => {
+export const insertCar = async (item: Car) => {
     const responseInsert = await ItemModel.create(item);
     return responseInsert;
 };
