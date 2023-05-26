9afe1f87a0116ea13a06769eb0bb1156094f7373
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-26 14:19:15 -0400

    Update and adding name

--- src/models/item.ts
+++ src/models/item.ts
@@ -3,21 +3,30 @@ import { Car } from "../interfaces/car.interface";
 
 const itemSchema = new Schema<Car>(
     {
+        name: {
+            type: String,
+            required: true,
+        },
         color: {
             type: String,
+            required: true,
         },
         gas: {
             type: String,
             enum: ["electric", "gasoline"],
+            required: true,
         },
         year: {
             type: Number,
+            required: true,
         },
         description: {
             type: String,
+            required: true,
         },
         price: {
             type: Number,
+            required: true,
         },
     },
     {
