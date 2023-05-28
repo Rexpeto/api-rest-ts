544c14bcdec95763d5a4bc0186264efd2013a14f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-28 09:52:34 -0400

    Save user in request

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,16 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { verifiedToken } from "../utils/jwt.handle";
+import { JwtPayload } from "jsonwebtoken";
 
-export const checkJWT = (req: Request, res: Response, next: NextFunction) => {
+interface RequestExt extends Request {
+    user?: string | JwtPayload;
+}
+
+export const checkJWT = (req: RequestExt, res: Response, next: NextFunction) => {
     try {
         const jwtUser = req.headers.authorization || '';
 
         if (!jwtUser) return res.status(403).json({ msg: "Debe tener una sesión abierta" });
 
         const jwt = jwtUser.split(' ').pop();
-        const isOk = verifiedToken(`${jwt}`);
+        const isUser = verifiedToken(`${jwt}`);
 
-        if (isOk) {
+        if (isUser) {
+            req.user = isUser;
             next();
         } else {
             res.status(403).json({ msg: "Token inválido" })
